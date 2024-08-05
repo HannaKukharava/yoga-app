a9219573214336faa462221cf38b29137481268a
Hanna Kukharava <hanna.kukharava@gmail.com>
2024-08-05 22:48:14 +0200

    Add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './modules/+core/containers/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./modules/+core/pages/home-page/home-page.component'),
+      },
+    ],
+  },
+];
